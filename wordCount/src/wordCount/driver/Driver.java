7a14ce1edd80c6f7a8ce4e943b6bc6588fa8d7c2
dmorris4 <dmorris4@binghamton.edu>
2015-04-20 15:57:13 -0400

    Added Command Line Input to Driver

--- wordCount/src/wordCount/driver/Driver.java
+++ wordCount/src/wordCount/driver/Driver.java
@@ -1,15 +1,29 @@
 package wordCount.driver;
 import java.util.TreeMap;
+import java.lang.NumberFormatException;
 import wordCount.treesForStrings.StringWrapper;
 import wordCount.treesForStrings.WordSizeNode;
 import wordCount.visitors.WordCountVisitor;
 import wordCount.visitors.Visitor;
 
 public class Driver{
-	public static final int NUM_ITERATIONS = 1;
 
 	public static void main(String args[]){
 		//Command Line Input
+		String inName = args[0];
+		String outName = args[1];
+		int iterations = 1;
+		int debug = 0;
+		try{
+			iterations = Integer.parseInt(args[2]);
+			debug = Integer.parseInt(args[3]);
+		}
+		catch(NumberFormatException e){
+			System.err.println("Iterations or Debug not ints, reverting to defaults");
+			iterations = 1;
+			debug = 0;
+		}
+		
 		//Start Timing
 		long startTime = System.currentTimeMillis();
 		for(int i=0;i<NUM_ITERATIONS;i++){
