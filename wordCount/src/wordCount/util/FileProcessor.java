309dafd46ca173118bbcae2a1a8ec0adf3b8fcc3
Adam Almonte <aalmont4@binghamton.edu>
2015-04-21 20:48:03 -0400

    wrote FileProcessor.java and started PopulateTreeVisitor.java

--- wordCount/src/wordCount/util/FileProcessor.java
+++ wordCount/src/wordCount/util/FileProcessor.java
@@ -1,9 +1,81 @@
 package wordCount.util;
-import java.io.*;
 
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+
+//import taskManager.util.MyLogger;
+/**
+* FileProcessor.java
+*
+* Class containing methods to read from a file and write to a file.
+*
+* @author Adam Almonte <aalmont4>
+*/
 public class FileProcessor{
-	
-	//public FileProcessor(
-	
-	//public String readLine(FILE 
+	Scanner sc;
+	String inputFile = "";
+	String outputFile = "";
+
+	//MyLogger logRef = MyLogger.getInstance();
+
+	/**
+	* Constructor for FileProcessor
+	* @param inFileName - the file to read from
+	* @param outFileName - the file to write to
+	*/
+	public FileProcessor(String inFileName, String outFileName){
+		inputFile = inFileName;
+		outputFile = outFileName;
+		try{
+			sc = new Scanner(new File(inFileName));
+
+		} catch (FileNotFoundException e){
+			//logRef.printMessage(0, "FileProcessor has thrown a FileNotFoundException. inputFile does not exist. Exiting program.");
+			//System.exit(1);
+		} catch (IOException e){
+			//logRef.printMessage(0, "FileProcessor has thrown an IOException. Error trying to write to file. Exiting program.");
+			//System.exit(1);
+		}
+		
+	}
+
+	/**
+	* Gets the next line from the input file
+	* @return Either the next line in the file, or the string "No lines left", signifying that the end of the file
+	*	has been reached
+	*/
+	public String getNextLineFromFile(){
+		//logRef.printMessage(3, "getNextLineFromFile() called");
+		if (sc.hasNextLine()){
+			return sc.nextLine();
+		} else{
+			return "No lines left";
+		}
+	}
+
+	/**
+	* Appends a line to the end of the output file.
+	* @param line - the line to be written
+	* @param printNewLine - if true, a newline character will be inserted at the end of line. 
+	*/
+	public void writeLineToFile(String line, boolean printNewLine){
+		//logRef.printMessage(3, "writeLineToFile() called");
+		try{
+			BufferedWriter bw = new BufferedWriter(new FileWriter(outputFile, true));
+			bw.write(line);
+			if(printNewLine == true){
+				bw.newLine();
+			}
+			bw.close();
+		} catch (IOException e){
+			//System.out.println("Error in FileProcessor: IOException writing to file.");
+			//logRef.printMessage(0, "FileProcessor has thrown an IOException. Error trying to write to file. Exiting program.");
+			//System.exit(1);
+		}
+	}
+
 }
