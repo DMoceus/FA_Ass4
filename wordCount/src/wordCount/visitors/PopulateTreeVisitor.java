824699717606493034db4ceb5318bd4b25e2dca1
dmorris4 <dmorris4@binghamton.edu>
2015-04-21 20:57:04 -0400

    Constructor for PopulateTreeVisitor

--- wordCount/src/wordCount/visitors/PopulateTreeVisitor.java
+++ wordCount/src/wordCount/visitors/PopulateTreeVisitor.java
@@ -1,5 +1,10 @@
 package wordCount.visitors;
+import java.util.TreeMap;
+import wordCount.treesForStrings.WordSizeNode;
 
 public class PopulateTreeVisitor implements Visitor{
-	
+	public TreeMap<String,WordSizeNode> tree;
+	public PopulateTreeVisitor(TreeMap<String,WordSizeNode> treeIn){
+		tree = treeIn;
+	}
 }
