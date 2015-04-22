613055647c47fbd8f98193b0913876ad080267af
Adam Almonte <aalmont4@binghamton.edu>
2015-04-21 21:30:48 -0400

    Finished(?) PopulateTreeVisitor.java

--- wordCount/src/wordCount/visitors/PopulateTreeVisitor.java
+++ wordCount/src/wordCount/visitors/PopulateTreeVisitor.java
@@ -11,7 +11,14 @@ public class PopulateTreeVisitor implements Visitor{
 	}
 
 	//read words from a file and populate the tree
-	public void visit(){
-		
+	public void visit(StringWrapper o){
+		//take the string and store it in the tree map
+		String s = o.getString();
+		if (tree.containsKey(s)){
+			(tree.get(s)).incrementCount();
+		} else{
+			WordSizeNode wsn = new WordSizeNode(s);
+			tree.put(s, wsn);
+		}
 	}
 }
